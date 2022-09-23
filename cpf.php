9467ceba66b4ea4e4f05550c4480ded81f9a9a5b
otaldopira <ericdenkievicz@hotmail.com>
2022-09-22 22:10:25 -0300

    Att Validações Servidor

---
+++ cpf.php
@@ -0,0 +1,12 @@
+<?php
+
+    $cpf = "105.365.699-82";
+
+    
+
+    $email = 'ericdenkievicz@<button?.com';
+
+     else {
+        echo 'Valido';
+    }
+
--- model/model.cadastro.php
+++ model/model.cadastro.php
@@ -8,20 +8,68 @@ if (isset($_POST['submit'])) {
      $celularUser = $_POST['celularUser'];
      $emailUser = $_POST['emailUser'];
      $senhaUser = $_POST['senhaUser'];
+     $erro = 0;
 
-     var_dump($_POST['nomeUser']);
-
+     //VERIFICAR SE TODOS OS INPUTS ESTÃO PREENCHIDOS
      if (!empty($nomeUser) && !empty($sobrenomeUser) && !empty($cpfUser) && !empty($celularUser) && !empty($emailUser) && !empty($senhaUser)) {
 
-          //Não deixa colocar caracter especial no nome.
+          //Não deixa colocar carácter especial no nome.
           if (!preg_match("/^[a-zA-Z-' ]*$/", $nomeUser)) {
+               $erro++;
                die('Caractere indisponível !');
           }
-          //Não deixa colocar caracter especial no sobrenome.
+          //Não deixa colocar carácter especial no sobrenome.
           if (!preg_match("/^[a-zA-Z-' ]*$/", $sobrenomeUser)) {
+               $erro++;
                die('Caractere indisponível !');
           }
 
+          //TIRA A MÁSCARA DO CPF
+          $cpfNoMask = preg_replace('/\D/', '', $cpfUser);
+
+          if (strlen($cpfNoMask) != 11) {
+               $erro++;
+               die ('CPF FALTANDO DÍGITOS!');
+          }
+
+          $d1 = 0;
+          $d2 = 0;
+
+          //VALIDANDO OS DÍGITOS VERIFICADORES DO CPF
+          if (preg_match('/(\d)\1{10}/', $cpfNoMask)) {
+               $erro++;
+               die("CPF INVÁLIDO!");
+          }
+
+          for ($i = 0, $x = 10; $i < 9; $i++, $x--) {
+               $d1 += $cpfNoMask[$i] * $x;
+          }
+
+          for ($i = 0, $x = 11; $i < 10; $i++, $x--) {
+               $d2 += $cpfNoMask[$i] * $x;
+          }
+
+          $tempD1 = (($d1 % 11) < 2) ? 0 : (11 - ($d1 % 11));
+          $tempD2 = (($d2 % 11) < 2) ? 0 : (11 - ($d2 % 11));
+
+          if ($tempD1 == $cpfNoMask[9] && $tempD2 == $cpfNoMask[10]) {
+               echo ($cpf);
+          } else {
+               $erro++;
+               die("CPF INVÁLIDO!");
+          }
+
+          //VALIDANDO E-MAIL
+          if (!filter_var($emailUser, FILTER_VALIDATE_EMAIL)) {
+               $erro++;
+               die("E-MAIL INVÁLIDO");
+          }
+     } else {
+          $erro++;
+          die('Obrigatório preencher todos os campos!');
+     }
+
+     if ($erro == 0) {
           $addUser = array("NOME" => "$nomeUser", "SOBRENOME" => "$sobrenomeUser", "CPF" => "$cpfUser", "CELULAR" => "$celularUser", "EMAIL" => "$emailUser", "SENHA" => "$senhaUser", "BEBUMCOINS" => 50000);
 
           $usuarios = array(
@@ -33,8 +81,5 @@ if (isset($_POST['submit'])) {
           echo "<pre>";
           print_r($usuarios);
           echo "</pre>";
-     } else {
-
-          die('Obrigatório preencher todos os campos!');
      }
 }
